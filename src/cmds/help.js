aba35b95cd53f1feb18da6c23674c65f941e93bf
Kevin Hallett <kevinmhallett@gmail.com>
2019-01-22 08:57:35 -0600

    add version aliases to help

--- src/cmds/help.js
+++ src/cmds/help.js
@@ -3,7 +3,7 @@ usage: automouse [command]
 
 Commands:
 interactive|i - starts interactive mode
-version - displays the current version.
+version|--version|-v - displays the current version.
 `;
 
 module.exports = args => console.log(helpMessage);

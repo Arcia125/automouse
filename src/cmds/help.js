15dd0f3c6ae5ce49ec1089c11f49f1f3e61750b2
Kevin Hallett <kevinmhallett@gmail.com>
2019-01-22 15:58:22 -0600

    update help

--- src/cmds/help.js
+++ src/cmds/help.js
@@ -4,6 +4,7 @@ usage: automouse [command]
 Commands:
 interactive|i - starts interactive mode
 version - displays the current version.
+file|f [path] - runs mouse commands from a file.
 `;
 
 // eslint-disable-next-line no-unused-vars, no-console
